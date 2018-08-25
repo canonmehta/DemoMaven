e39e2349de2d659586c2dd97c14291cebf3f3799
Canon Mehta <canonpce115@poornima.org>
2018-08-25 20:38:41 +0530

    values

--- src/main/java/com/heroku/maven/MyServlet.java
+++ src/main/java/com/heroku/maven/MyServlet.java
@@ -1,6 +1,10 @@
 package com.heroku.maven;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,7 +31,18 @@ public class MyServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		List<String> list1 = new ArrayList<>();
+
+		list1.add("Demo 1");
+		list1.add("Demo 2");
+		list1.add("Canon 1");
+		list1.add("Canon 2");
+
+		String valuesWith2 = list1.stream().filter(x -> x.contains("2")).collect(Collectors.joining());
+
 		response.getWriter().append("<html><body><h1>Dummy Servlet served !!! :)</h1></body></html>");
+		response.getWriter().append("<html><body><h1>Values !!! :)" + valuesWith2 + "</h1></body></html>");
 	}
 
 	/**
